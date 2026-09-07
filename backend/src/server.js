c346d9fcd94d5d3f374f3abd00ca2e35f4fa44b1
AkmalD <goniyyu@gmail.com>
2026-09-07 22:42:49 +0700

    [ADD] Backend codebase

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/api/health', (req, res) => {
+	res.json({
+		success: true,
+		message: 'Success',
+		data: {},
+	});
+});
+
+app.listen(port, () => {
+	console.log(`Server running on port ${port}`);
+});
